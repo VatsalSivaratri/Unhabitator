75d888b2b9c1dc7919d97578fe6b6764c750ea9a
VatsalSivaratri <vatsal.sivaratri@gmail.com>
2023-03-04 14:09:52 -0500

    Initial Commit

---
+++ Unhabitator/UnhabitatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnhabitatorApp.swift
+//  Unhabitator
+//
+//  Created by Vatsal Sivaratri on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UnhabitatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
